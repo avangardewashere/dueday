e4c14b7f1738db2ef3d5ed1cb8f304687a82b794
avangardewashere <avelpanaligan@gmail.com>
2024-12-29 23:02:10 +0800

    add/home-route-and--initial-layout

---
+++ app/home/page.tsx
@@ -0,0 +1,15 @@
+import clsx from "clsx";
+import { memo } from "react";
+import style from "./index.module.scss";
+
+const Home = memo(() => {
+  return (
+    <div className={clsx(style.container)}>
+      <div className={clsx(style.header)}></div>
+      <div className={clsx(style.body)}></div>
+      <div className={clsx(style.footer)}></div>
+    </div>
+  );
+});
+
+export default Home;
