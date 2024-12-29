b42a09defe0da1a1e2865cfe7907dc26ba3271cf
avangardewashere <avelpanaligan@gmail.com>
2024-12-29 22:58:18 +0800

    add/structure-of-login

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,12 +1,26 @@
-import clsx from 'clsx';
-import { memo } from 'react';
-import style from './index.module.scss';
+import clsx from "clsx";
+import { memo } from "react";
+import style from "./index.module.scss";
 
 const ComponentName = memo(() => {
   return (
     <div className={clsx(style.container)}>
-      <div className={style.ImageSample}>
-    hello
+      <div className={clsx(style.splashItem)}>
+        <div className={style.imageSample}></div>
+        <div className={clsx(style.caption)}>
+          <h2>Create Good Habits</h2>
+          <span>
+            Change your life by slowly adding new healthy habits and sticking to
+            them
+          </span>
+        </div>
+      </div>
+
+      <div className={clsx(style.buttonsWrapper)}>
+        <div className={clsx(style.button)}>COntinue with Email</div>
+      </div>
+      <div className={style.message}>
+        By Continue you agree to Terms of Service & Privacy Policy
       </div>
     </div>
   );
