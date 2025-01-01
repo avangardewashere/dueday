c198795e5bf811ddc87e9879a1add6c042322398
avangardewashere <avelpanaligan@gmail.com>
2025-01-01 16:11:14 +0800

    add/splash-data-and-swiper

--- app/(auth)/screen/page.tsx
+++ app/(auth)/screen/page.tsx
@@ -1,38 +1,46 @@
+"use client";
 import clsx from "clsx";
 import { memo } from "react";
 import style from "./index.module.scss";
 import splashOne from "@/assets/images/splash/splash-pic.png";
 import Image from "next/image";
 import WhiteButton from "@/components/Fragments/button";
-import { buttonData } from "@/types/data/home";
-
+import { buttonData, splashData } from "@/types/data/home";
+import { Swiper } from "antd-mobile";
 
 const ComponentName = memo(() => {
-
-
   return (
     <div className={clsx(style.container)}>
-      <div className={clsx(style.splashItem)}>
-        <Image className={style.imageSample} src={splashOne} alt="" />
-        <div className={clsx(style.caption)}>
-          <h2>
-            Create <br /> Good Habits
-          </h2>
-          <span>
-            Change your life by slowly adding new healthy habits and sticking to
-            them
-          </span>
-        </div>
-      </div>
+      <Swiper loop autoplay autoplayInterval={5000}>
+        {splashData.map((item) => (
+          <Swiper.Item key={item.id}>
+            <div className={clsx(style.splashItem)}>
+              <Image
+                className={style.imageSample}
+                src={item.img}
+                alt={`image: ${item.title}`}
+              />
+              <div className={clsx(style.caption)}>
+                <h2>{item.title}</h2>
+                <span>{item.description}</span>
+              </div>
+            </div>
+          </Swiper.Item>
+        ))}
+      </Swiper>
 
       <div className={clsx(style.buttonsWrapper)}>
-        {buttonData.map((button)=>{
-          return(
-            <WhiteButton image={button.image} title={button.title} width={button.width} height={button.height} />
-          )
+        {buttonData.map((button) => {
+          return (
+            <WhiteButton
+              key={button.id}
+              image={button.image}
+              title={button.title}
+              width={button.width}
+              height={button.height}
+            />
+          );
         })}
-    
-        
 
         <div className={style.message}>
           By Continue you agree to Terms of Service & Privacy Policy
--- types/data/home.ts
+++ types/data/home.ts
@@ -2,7 +2,9 @@ import appleImg from "@/assets/images/icons/apple.png";
 import googleImg from "@/assets/images/icons/google.png";
 import facebookImg from "@/assets/images/icons/facebook.png";
 import loginImg from "@/assets/images/icons/Login.png";
-
+import splashOne from "@/assets/images/splash/splash-pic.png";
+import splashTwo from "@/assets/images/splash/splash-pic2.png";
+import splashThree from "@/assets/images/splash/splash-3.png";
 export const buttonData = [
   {
     title: "Continue with Email",
@@ -33,4 +35,27 @@ export const buttonData = [
     height: undefined,
   },
 ];
- 
+
+export const splashData = [
+  {
+    id: 111,
+    title: "Create Good Habits",
+    description:
+      "Change your life by slowly adding new healty habits and sticking to them.",
+    img: splashOne,
+  },
+  {
+    id: 112,
+    title: "Track Your Progress",
+    description:
+      "Everyday you become one step closer to your goal. Don't give up!",
+    img: splashTwo,
+  },
+  {
+    id: 113,
+    title: "Stay Together and Strong",
+    description:
+      "Find friends to discuss common topics. Complete challenges together.",
+    img: splashThree,
+  },
+];
