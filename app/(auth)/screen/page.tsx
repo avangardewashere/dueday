69292b3f59d8da8c076972faa7db65fb5d3addf0
avangardewashere <avelpanaligan@gmail.com>
2025-01-01 14:51:47 +0800

    splashItem

--- app/(auth)/screen/page.tsx
+++ app/(auth)/screen/page.tsx
@@ -1,14 +1,15 @@
 import clsx from "clsx";
 import { memo } from "react";
 import style from "./index.module.scss";
-
+import splashOne from "@/assets/images/splash/splash-pic.png"
+import Image from "next/image";
 const ComponentName = memo(() => {
   return (
     <div className={clsx(style.container)}>
       <div className={clsx(style.splashItem)}>
-        <div className={style.imageSample}></div>
+        <Image className={style.imageSample} src={splashOne}  alt=""/>
         <div className={clsx(style.caption)}>
-          <h2>Create Good Habits</h2>
+          <h2>Create <br /> Good Habits</h2>
           <span>
             Change your life by slowly adding new healthy habits and sticking to
             them
