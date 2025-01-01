0fef7949db0e01e30ef1ddfdf688e66bd0567c6b
avangardewashere <avelpanaligan@gmail.com>
2025-01-01 17:16:31 +0800

    add/login-form-design

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,36 +1,44 @@
 "use client";
 import clsx from "clsx";
-import { memo } from "react";
+import { memo, useState } from "react";
 import style from "./index.module.scss";
 import NavHeader from "@/components/Fragments/NavHeader";
 import Image from "next/image";
 import removeImg from "@/assets/images/icons/removeIcon.png";
 const LoginPage = memo(() => {
+
+    const [email,setEmail] = useState("")
+    const [pass,setPass] = useState("")
+
   return (
     <div className={clsx(style.container)}>
       <NavHeader title="Continue with E-mail" />
       <div className={style.content}>
         <form action="" className={style.form}>
-          <div className={style.inputItem}>
-            <label htmlFor="email">E-mail</label>
-            <input type="email" name="email" placeholder="Enter your Email" />
-            <Image src={removeImg} alt="" />
-          </div>
-          <div className={style.inputItem}>
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              placeholder="Enter your password"
-            />
+          <div className={style.inputFields}>
+            <div className={style.inputItem}>
+              <label htmlFor="email">E-mail</label>
+              <input type="email" name="email" placeholder="Enter your Email" />
+              <Image className={style.removeIcon} src={removeImg} alt="" />
+            </div>
+            <div className={style.inputItem}>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Enter your password"
+                autoComplete="off"
+              />
 
-            <span className={style.forgotPass}>forgot your password</span>
+              <Image className={style.removeIcon} src={removeImg} alt="" />
+              <span className={style.forgotPass}>forgot your password</span>
+            </div>
           </div>
 
           <div className={style.formFooter}>
             <span>Don't have an account Yet, Register Here</span>
             <button className={style.submitButton} type="submit">
-              Submit
+              Next
             </button>
           </div>
         </form>
